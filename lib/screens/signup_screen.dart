a0f84d917bfe73fef3e14ae23df1d2bda50d6cde
anshulshacks <sinha.anshul1@gmail.com>
2021-04-24 17:05:38 -0700

    added picking profile picture

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/material.dart';
 import 'package:frenzie/screens/friends_screen.dart';
+import 'package:frenzie/widgets/profile_picker.dart';
 
 class SignupScreen extends StatefulWidget {
   @override
@@ -29,239 +30,252 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              'Signup',
-              style: TextStyle(
-                fontFamily: 'Mogra',
-                fontWeight: FontWeight.bold,
-                fontSize: 36,
-                color: Theme.of(context).accentColor,
-                shadows: [
-                  Shadow(
-                    offset: Offset(1.0, 1.0),
-                    blurRadius: 3.0,
-                    color: Color.fromRGBO(255, 222, 49, 1),
-                  ),
-                ],
-              ),
+    return Scaffold(body: Builder(
+      builder: (BuildContext context1) {
+        return Column(
+          children: [
+            SizedBox(
+              height: 100,
             ),
-          ),
-          SizedBox(height: 30),
-          Container(
-            padding: EdgeInsets.only(
-              left: 30,
-            ),
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Email',
-              style: TextStyle(
-                fontFamily: 'Mogra',
-                color: Theme.of(context).accentColor,
-                fontSize: 18,
-                shadows: [
-                  Shadow(
-                    offset: Offset(1.0, 1.0),
-                    blurRadius: 3.0,
-                    color: Color.fromRGBO(255, 222, 49, 1),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 15,
-              horizontal: 30,
-            ),
-            child: TextField(
-              controller: emailController,
-              style: TextStyle(
-                color: Theme.of(context).accentColor,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: Theme.of(context).primaryColor),
-                prefixIcon: Icon(
-                  Icons.mail,
-                  color: Theme.of(context).primaryColor,
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Signup',
+                style: TextStyle(
+                  fontFamily: 'Mogra',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                  color: Theme.of(context).accentColor,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 3.0,
+                      color: Color.fromRGBO(255, 222, 49, 1),
+                    ),
+                  ],
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.only(
+                left: 30,
+              ),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Email',
+                style: TextStyle(
+                  fontFamily: 'Mogra',
+                  color: Theme.of(context).accentColor,
+                  fontSize: 18,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 3.0,
+                      color: Color.fromRGBO(255, 222, 49, 1),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 30,
+              ),
+              child: TextField(
+                controller: emailController,
+                style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+                  prefixIcon: Icon(
+                    Icons.mail,
                     color: Theme.of(context).primaryColor,
-                    width: 3,
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: Theme.of(context).accentColor,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: 3,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).accentColor,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 30),
-          Container(
-            padding: EdgeInsets.only(
-              left: 30,
-            ),
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'Password',
-              style: TextStyle(
-                fontFamily: 'Mogra',
-                color: Theme.of(context).accentColor,
-                fontSize: 18,
-                shadows: [
-                  Shadow(
-                    offset: Offset(1.0, 1.0),
-                    blurRadius: 3.0,
-                    color: Color.fromRGBO(255, 222, 49, 1),
-                  ),
-                ],
+            SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.only(
+                left: 30,
               ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 15,
-              horizontal: 30,
-            ),
-            child: TextField(
-              obscureText: true,
-              controller: passwordController,
-              style: TextStyle(
-                color: Theme.of(context).accentColor,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Theme.of(context).primaryColor),
-                prefixIcon: Icon(
-                  Icons.lock,
-                  color: Theme.of(context).primaryColor,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Password',
+                style: TextStyle(
+                  fontFamily: 'Mogra',
+                  color: Theme.of(context).accentColor,
+                  fontSize: 18,
+                  shadows: [
+                    Shadow(
+                      offset: Offset(1.0, 1.0),
+                      blurRadius: 3.0,
+                      color: Color.fromRGBO(255, 222, 49, 1),
+                    ),
+                  ],
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 30,
+              ),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+                  prefixIcon: Icon(
+                    Icons.lock,
                     color: Theme.of(context).primaryColor,
-                    width: 3,
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: Theme.of(context).accentColor,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: 3,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).accentColor,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          // Row(
-          //   children: [
-          //     DropdownButton<String>(
-          //       value: interest_1,
-          //       icon: const Icon(Icons.arrow_downward),
-          //       iconSize: 24,
-          //       elevation: 16,
+            ProfilePicker(),
+            // Row(
+            //   children: [
+            //     DropdownButton<String>(
+            //       value: interest_1,
+            //       icon: const Icon(Icons.arrow_downward),
+            //       iconSize: 24,
+            //       elevation: 16,
 
-          //       items: interests.map<DropdownMenuItem<String>>((String value) {
-          //         return DropdownMenuItem<String>(
-          //           value: value,
-          //           child: Text(value),
-          //         );
-          //       }).toList(),
-          //       onChanged: null,
-          //     ),
-          //     DropdownButton(
-          //       value: interest_2,
-          //       items: interests.map<DropdownMenuItem<String>>((String value) {
-          //         return DropdownMenuItem<String>(
-          //           value: value,
-          //           child: Text(value),
-          //         );
-          //       }).toList(),
-          //       onChanged: null,
-          //     ),
-          //     DropdownButton(
-          //       value: interest_3,
-          //       items: interests.map<DropdownMenuItem<String>>((String value) {
-          //         return DropdownMenuItem<String>(
-          //           value: value,
-          //           child: Text(value),
-          //         );
-          //       }).toList(),
-          //       onChanged: null,
-          //     ),
-          //   ],
-          // ),
-          RaisedButton(
-            color: Theme.of(context).accentColor,
-            child: Text(
-              'Sign up',
-              style: TextStyle(
-                fontFamily: 'Mogra',
-                color: Colors.white,
+            //       items: interests.map<DropdownMenuItem<String>>((String value) {
+            //         return DropdownMenuItem<String>(
+            //           value: value,
+            //           child: Text(value),
+            //         );
+            //       }).toList(),
+            //       onChanged: null,
+            //     ),
+            //     DropdownButton(
+            //       value: interest_2,
+            //       items: interests.map<DropdownMenuItem<String>>((String value) {
+            //         return DropdownMenuItem<String>(
+            //           value: value,
+            //           child: Text(value),
+            //         );
+            //       }).toList(),
+            //       onChanged: null,
+            //     ),
+            //     DropdownButton(
+            //       value: interest_3,
+            //       items: interests.map<DropdownMenuItem<String>>((String value) {
+            //         return DropdownMenuItem<String>(
+            //           value: value,
+            //           child: Text(value),
+            //         );
+            //       }).toList(),
+            //       onChanged: null,
+            //     ),
+            //   ],
+            // ),
+            RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text(
+                'Sign up',
+                style: TextStyle(
+                  fontFamily: 'Mogra',
+                  color: Colors.white,
+                ),
               ),
+              onPressed: () async {
+                FirebaseAuth auth = FirebaseAuth.instance;
+                AuthResult _authResult;
+
+                _authResult = await auth.createUserWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text);
+                var currentUUID = '';
+
+                void getUUID() async {
+                  final FirebaseUser user = await auth.currentUser();
+                  currentUUID = user.uid;
+                  print(currentUUID);
+                  print(currentUUID);
+
+                  Firestore.instance.collection('profile').add({
+                    'uuid': currentUUID,
+                    'first_name': null,
+                    'last_name': null,
+                    'interest_1': null,
+                    'interest_2': null,
+                    'interest_3': null,
+                    'age': null,
+                    'pfp': null,
+                  });
+                }
+
+                getUUID();
+                Scaffold.of(context1).showSnackBar(SnackBar(
+                  content: Text(
+                    'Update your profile on the profile page',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  elevation: 5,
+                  backgroundColor: Color.fromRGBO(92, 83, 70, 1),
+                ));
+                print(currentUUID);
+
+                // Firestore.instance.collection('profile').add({
+                //   'uuid': currentUUID,
+                //   'first_name': null,
+                //   'last_name': null,
+                //   'interest_1': null,
+                //   'interest_2': null,
+                //   'interest_3': null,
+                //   'age': null,
+                //   'pfp': null,
+                // });
+                print(currentUUID);
+                print('ready to do it');
+
+                Navigator.of(context).pushNamed(
+                  Friends.routeName,
+                );
+              },
             ),
-            onPressed: () async {
-              FirebaseAuth auth = FirebaseAuth.instance;
-              AuthResult _authResult;
-
-              _authResult = await auth.createUserWithEmailAndPassword(
-                  email: emailController.text,
-                  password: passwordController.text);
-              var currentUUID = '';
-
-              void getUUID() async {
-                final FirebaseUser user = await auth.currentUser();
-                currentUUID = user.uid;
-                print(currentUUID);
-                print(currentUUID);
-
-                Firestore.instance.collection('profile').add({
-                  'uuid': currentUUID,
-                  'first_name': null,
-                  'last_name': null,
-                  'interest_1': null,
-                  'interest_2': null,
-                  'interest_3': null,
-                  'age': null,
-                  'pfp': null,
-                });
-              }
-
-              getUUID();
-              print(currentUUID);
-
-              // Firestore.instance.collection('profile').add({
-              //   'uuid': currentUUID,
-              //   'first_name': null,
-              //   'last_name': null,
-              //   'interest_1': null,
-              //   'interest_2': null,
-              //   'interest_3': null,
-              //   'age': null,
-              //   'pfp': null,
-              // });
-              print(currentUUID);
-              print('ready to do it');
-              
-              Navigator.of(context).pushNamed(
-                Friends.routeName,
-              );
-            },
-          ),
-        ],
-      ),
-    );
+          ],
+        );
+      },
+    ));
   }
 }
---
+++ lib/widgets/profile_picker.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePicker extends StatefulWidget {
+  @override
+  _ProfilePickerState createState() => _ProfilePickerState();
+}
+
+class _ProfilePickerState extends State<ProfilePicker> {
+  File _pickedProfilePicture;
+  void imagePicker() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+    final pickedImageFile = File(pickedImage.path);
+    setState(() {
+      _pickedProfilePicture = pickedImageFile;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(radius: 40, backgroundImage: _pickedProfilePicture != null ? FileImage(_pickedProfilePicture) : NetworkImage('https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png')),
+        FlatButton.icon(
+          onPressed: imagePicker,
+          icon: Icon(Icons.image),
+          label: Text('Choose a profile picture'),
+        ),
+      ],
+    );
+  }
+}
